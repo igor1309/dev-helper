f1f30ece71869e79be2e3a808e077e8666cd9f4f
Igor Malyarov <igor1309@gmail.com>
2021-09-12 11:52:07 +0300

    BundleImage enum

--- Sources/dev-helper/UIImage+images.swift
+++ Sources/dev-helper/UIImage+images.swift
@@ -11,9 +11,9 @@ import UIKit
 
 extension UIImage {
     
-    public static let autumn = UIImage(named: "autumn.jpeg", in: .module, with: nil)!
-    public static let autumn2 = UIImage(named: "autumn2.jpeg", in: .module, with: nil)!
-
+    public static let autumn: UIImage = UIImage(named: "autumn.jpeg", in: .module, with: nil)!
+    public static let autumn2: UIImage = UIImage(named: "autumn2.jpeg", in: .module, with: nil)!
+    
     public static let flower: UIImage = UIImage(named: "flower.jpeg", in: .module, with: nil)!
     
     public static let leaf: UIImage = UIImage(named: "leaf.jpeg", in: .module, with: nil)!
@@ -23,10 +23,79 @@ extension UIImage {
     
     public static let mguAutumn: UIImage = UIImage(named: "mgu_autumn.jpeg", in: .module, with: nil)!
     public static let mguHuge: UIImage = UIImage(named: "IMG_2061.jpeg", in: .module, with: nil)!
-    public static let mguSun = UIImage(named: "mgu_sun.jpeg", in: .module, with: nil)!
+    public static let mguSun: UIImage  = UIImage(named: "mgu_sun.jpeg", in: .module, with: nil)!
     
     public static let sea: UIImage = UIImage(named: "sea.jpeg", in: .module, with: nil)!
     
     public static let numGrid: UIImage = UIImage(named: "numgrid.png", in: .module, with: nil)!
+    
+}
+
+public enum BundleImage: String, CaseIterable {
+    
+    case autumn = "Autumn"
+    case autumn2 = "Autumn 2"
+    case flower = "Flower"
+    case leaf = "Leaf"
+    case leaf2 = "Leaf 2"
+    case mediterraneanSea = "Mediterranean Sea"
+    case mguAutumn = "MGU Autumn"
+    case mguHuge = "MGU (huge)"
+    case mguSun = "MGU Sun"
+    case sea = "Sea"
+    case numGrid = "Numerical Grid"
+    
+    public var fileName: String {
+        switch self {
+        case .autumn:
+            return "autumn.jpeg"
+        case .autumn2:
+            return "autumn2.jpeg"
+        case .flower:
+            return "flower.jpeg"
+        case .leaf:
+            return "leaf.jpeg"
+        case .leaf2:
+            return "leaf2.jpeg"
+        case .mediterraneanSea:
+            return "mediterranean-sea.jpg"
+        case .mguAutumn:
+            return "mgu_autumn.jpeg"
+        case .mguHuge:
+            return "IMG_2061.jpeg"
+        case .mguSun:
+            return "mgu_sun.jpeg"
+        case .sea:
+            return "sea.jpeg"
+        case .numGrid:
+            return "numgrid.png"
+        }
+    }
+    
+    public var icon: String {
+        switch self {
+            // case .autumn:
+            // case .autumn2:
+            // case .flower:
+        case .leaf:
+            return "leaf"
+        case .leaf2:
+            return "leaf"
+            // case .mediterraneanSea:
+            // case .mguAutumn:
+            // case .mguHuge:
+        case .mguSun:
+            return "sun.max"
+            // case .sea:
+        case .numGrid:
+            return "square.grid.4x3.fill"
+        default:
+            return "photo"
+        }
+    }
+    
+    public var uiImage: UIImage? {
+        UIImage(named: fileName, in: .module, with: nil)
+    }
     
 }
--- Tests/dev-helperTests/UIImage+imagesTests.swift
+++ Tests/dev-helperTests/UIImage+imagesTests.swift
@@ -5,20 +5,81 @@ final class UIImageImagesTests: XCTestCase {
     func testImagesAreNotNill() throws {
         XCTAssertNotNil(UIImage.autumn)
         XCTAssertNotNil(UIImage.autumn2)
-
+        
         XCTAssertNotNil(UIImage.flower)
-
+        
         XCTAssertNotNil(UIImage.leaf)
         XCTAssertNotNil(UIImage.leaf2)
-
+        
         XCTAssertNotNil(UIImage.mediterraneanSea)
-
+        
         XCTAssertNotNil(UIImage.mguAutumn)
         XCTAssertNotNil(UIImage.mguHuge)
         XCTAssertNotNil(UIImage.mguSun)
-
+        
         XCTAssertNotNil(UIImage.sea)
-
+        
         XCTAssertNotNil(UIImage.numGrid)
     }
+    
+    func testBundleImageFileName() {
+        XCTAssertEqual(BundleImage.autumn.fileName, "autumn.jpeg")
+        XCTAssertEqual(BundleImage.autumn2.fileName, "autumn2.jpeg")
+        
+        XCTAssertEqual(BundleImage.flower.fileName, "flower.jpeg")
+        
+        XCTAssertEqual(BundleImage.leaf.fileName, "leaf.jpeg")
+        XCTAssertEqual(BundleImage.leaf2.fileName, "leaf2.jpeg")
+        
+        XCTAssertEqual(BundleImage.mediterraneanSea.fileName, "mediterranean-sea.jpg")
+        
+        XCTAssertEqual(BundleImage.mguAutumn.fileName, "mgu_autumn.jpeg")
+        XCTAssertEqual(BundleImage.mguHuge.fileName, "IMG_2061.jpeg")
+        XCTAssertEqual(BundleImage.mguSun.fileName, "mgu_sun.jpeg")
+        
+        XCTAssertEqual(BundleImage.sea.fileName, "sea.jpeg")
+        
+        XCTAssertEqual(BundleImage.numGrid.fileName, "numgrid.png")
+    }
+    
+    func testBundleImageFileIcon() {
+        XCTAssertEqual(BundleImage.autumn.icon, "photo")
+        XCTAssertEqual(BundleImage.autumn2.icon, "photo")
+        
+        XCTAssertEqual(BundleImage.flower.icon, "photo")
+        
+        XCTAssertEqual(BundleImage.leaf.icon, "leaf")
+        XCTAssertEqual(BundleImage.leaf2.icon, "leaf")
+        
+        XCTAssertEqual(BundleImage.mediterraneanSea.icon, "photo")
+        
+        XCTAssertEqual(BundleImage.mguAutumn.icon, "photo")
+        XCTAssertEqual(BundleImage.mguHuge.icon, "photo")
+        XCTAssertEqual(BundleImage.mguSun.icon, "sun.max")
+        
+        XCTAssertEqual(BundleImage.sea.icon, "photo")
+        
+        XCTAssertEqual(BundleImage.numGrid.icon, "square.grid.4x3.fill")
+    }
+    
+    func testBundleImageImage() {
+        XCTAssertNotNil(BundleImage.autumn.uiImage)
+        XCTAssertNotNil(BundleImage.autumn2.uiImage)
+        
+        XCTAssertNotNil(BundleImage.flower.uiImage)
+        
+        XCTAssertNotNil(BundleImage.leaf.uiImage)
+        XCTAssertNotNil(BundleImage.leaf2.uiImage)
+        
+        XCTAssertNotNil(BundleImage.mediterraneanSea.uiImage)
+        
+        XCTAssertNotNil(BundleImage.mguAutumn.uiImage)
+        XCTAssertNotNil(BundleImage.mguHuge.uiImage)
+        XCTAssertNotNil(BundleImage.mguSun.uiImage)
+        
+        XCTAssertNotNil(BundleImage.sea.uiImage)
+        
+        XCTAssertNotNil(BundleImage.numGrid.uiImage)
+    }
+    
 }
